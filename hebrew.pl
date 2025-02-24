9f97d7c3c06c908f9c2bb6e58bd6da82505cec99
Philippe Aucazou <paucazou@yahoo.fr>
2025-02-24 10:07:44 +0100

    tests

--- hebrew.pl
+++ hebrew.pl
@@ -3,60 +3,64 @@
 % Please note that this transliteration is not standard
 % but suits my own use
 
+:- module(hebrew, [
+                transliterate_word/3,
+                s/2
+                ]).
+
 % Define transliteration rules
-transliterate('A') --> [א].        % Aleph
-transliterate(b) --> [ב].        % Bet
-transliterate(g) --> [ג].        % Gimel
-transliterate(d) --> [ד].        % Dalet
-transliterate(h) --> [ה].        % He
-transliterate(w) --> [ו].        % Vav
-transliterate(z) --> [ז].        % Zayin
-transliterate(x) --> [ח].        % Het
-transliterate('T') --> [ט].      % Tet
-transliterate(y) --> [י].        % Yod
-transliterate(k) --> [כ].        % Kaf
-transliterate(l) --> [ל].        % Lamed
-transliterate(m) --> [מ].        % Mem
-transliterate(n) --> [נ].        % Nun
-transliterate(s) --> [ס].        % Samekh
-transliterate('H') --> [ע].      % Ayin
-transliterate(p) --> [פ].        % Pe
-transliterate('Z') --> [צ].      % Tsade
-transliterate(q) --> [ק].        % Qof
-transliterate(r) --> [ר].        % Resh
-transliterate(c) --> [שׁ].        % Shin
-transliterate('C') --> [שׂ].      % Sin
-transliterate(t) --> [ת].        % Tav
+% Please do not forget to use ' ' instead of " " or ` `
+transliterate('A') --> [א],!.      % Aleph
+transliterate(b) --> [ב],!.        % Bet
+transliterate(g) --> [ג],!.        % Gimel
+transliterate(d) --> [ד],!.        % Dalet
+transliterate(h) --> [ה],!.        % He
+transliterate(w) --> [ו],!.        % Vav
+transliterate(z) --> [ז],!.        % Zayin
+transliterate(x) --> [ח],!.        % Het
+transliterate('T') --> [ט],!.      % Tet
+transliterate(y) --> [י],!.        % Yod
+transliterate(k) --> [כ],!.        % Kaf
+transliterate(l) --> [ל],!.        % Lamed
+transliterate(m) --> [מ],!.        % Mem
+transliterate(n) --> [נ],!.        % Nun
+transliterate(s) --> [ס],!.        % Samekh
+transliterate('H') --> [ע],!.      % Ayin
+transliterate(p) --> [פ],!.        % Pe
+transliterate('Z') --> [צ],!.      % Tsade
+transliterate(q) --> [ק],!.        % Qof
+transliterate(r) --> [ר],!.        % Resh
+transliterate(c) --> [שׁ],!.        % Shin
+transliterate('C') --> [שׂ],!.      % Sin
+transliterate(t) --> [ת],!.        % Tav
 
 % Diacritics
-transliterate('.') --> [ּ].    % Dagesh
-transliterate(a) --> [ ַ].    % Patah
-transliterate(à) --> [ָ].    % Qamats
-transliterate(u) --> [ ֻ].        % Qubbuts
-transliterate(è) --> [ ֶ].        % Ségol
-transliterate(é) --> [ֵ].         % Tsere
-transliterate(i) --> [ ִ].        % Hiriq
-transliterate(o) --> [ ֹ].    % holam
-transliterate(â) --> [ֱ].  % Hateph Patah
-transliterate(ä) --> [ֳ].  % Hateph Qamats
-transliterate(ê) --> [ֲ].  % Hateph Segol
-transliterate(e) --> [ְ].  % Shewa
-transliterate('V') --> [֫].  % Ole 
+transliterate('.') --> [ּ],!.       % Dagesh
+transliterate(a) --> [ ַ],!.        % Patah
+transliterate(à) --> [ָ],!.         % Qamats
+transliterate(u) --> [ ֻ],!.        % Qubbuts
+transliterate(è) --> [ ֶ],!.        % Ségol
+transliterate(é) --> [ֵ],!.         % Tsere
+transliterate(i) --> [ ִ],!.        % Hiriq
+transliterate(o) --> [ ֹ],!.        % holam
+transliterate(â) --> [ֱ],!.         % Hateph Patah
+transliterate(ä) --> [ֳ],!.         % Hateph Qamats
+transliterate(ê) --> [ֲ],!.         % Hateph Segol
+transliterate(e) --> [ְ],!.         % Shewa
+transliterate('V') --> [֫],!.       % Ole 
 
-transliterate(R) --> [R].  % Keep every other character
+transliterate(R) --> [R].          % Keep every other character
 
 % Final letters
-% Please do not forget to use ' ' instead of " " or ` `
-transliterate_final(m, ' ') --> [ם].  % Final Mem
-transliterate_final(n, ' ') --> [ן].  % Final Nun
-transliterate_final(k, ' ') --> [ך].  % Final Kaf
-transliterate_final(p, ' ') --> [ף].  % Final Pe
-transliterate_final('Z', ' ') --> [ץ].% Final Tsade
+transliterate_final(m) --> [ם].    % Final Mem
+transliterate_final(n) --> [ן].    % Final Nun
+transliterate_final(k) --> [ך].    % Final Kaf
+transliterate_final(p) --> [ף].    % Final Pe
+transliterate_final('Z') --> [ץ].  % Final Tsade
 
 % Transliterate a full sentence
-transliterate_sentence([]) --> [].
-transliterate_sentence([H]) --> transliterate_final(H, ' '), !.
-transliterate_sentence([H,' '|T]) --> transliterate_final(H, ' '), transliterate_sentence(T),!.
-transliterate_sentence([H|T]) --> transliterate(H), transliterate_sentence(T).
+transliterate_word([]) --> [].
+transliterate_word([H]) --> transliterate_final(H), !.
+transliterate_word([H|T]) --> transliterate(H), transliterate_word(T).
 
-s(L, R) :- transliterate_sentence(L, LR, []), atomic_list_concat(LR, R).
+s(L, R) :- transliterate_word(L, LR, []), atomic_list_concat(LR, R).
---
+++ hebrew.plt
@@ -0,0 +1,29 @@
+% To run the tests, first load the module related to this one ;
+% next, run load_test_files(something_including_something_unrelated_to_the_module).
+:- use_module(hebrew).
+:- begin_tests(hebrew).
+test(transliterate_word, [true(A==[א, ב, ג, ד, ה, ו, ז, ח])]) :-
+    transliterate_word(['A', b, g, d, h, w, z, x], A, []).
+test(transliterate_word, [true(A==[פ, צ, ק, ר, שׁ, שׂ, ת])]) :-
+    transliterate_word([p, 'Z', q, r, c, 'C', t], A, []).
+test(transliterate_word, [true(A==[ט, י, כ, ל, מ, נ, ס, ע])]) :-
+    transliterate_word(['T', y, k, l, m, n, s, 'H'], A, []).
+
+test(final_letters, [true(A==[מ, ם])]) :-
+    transliterate_word([m, m], A, []).
+test(final_letters, [true(A==[נ, ן])]) :-
+    transliterate_word([n, n], A, []).
+test(final_letters, [true(A==[כ, ך])]) :-
+    transliterate_word([k, k], A, []).
+test(final_letters, [true(A==[פ, ף])]) :-
+    transliterate_word([p, p], A, []).
+test(final_letters, [true(A==[צ, ץ])]) :-
+    transliterate_word(['Z', 'Z'], A, []).
+
+test(diacritics, [true(A==[פ, 'ּ', פ, 'ַ', פ, 'ָ', פ, 'ֻ', פ, 'ֶ', פ, 'ֵ', פ, 'ִ', פ, 'ֹ', פ, 'ֱ', פ, 'ֳ', פ, 'ֲ', פ, 'ְ', פ, 'ְ', '֫'])]) :-
+    transliterate_word([p, '.', p, a, p, à, p, u, p, è, p, é, p, i, p, o, p, â, p, ä, p, ê, p, e, p, e, 'V'],
+                       A,
+                       []).
+
+
+:- end_tests(hebrew).
